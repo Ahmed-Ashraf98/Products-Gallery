c48d4439bcab843b7109a72c8c72e1218be269c5
Ahmed Ashraf <a.ashraf.elbendary@gmail.com>
2025-07-26 15:40:56 +0300

    Fix ProductCard issues

--- src/app/features/product/components/productCard/productCard.component.ts
+++ src/app/features/product/components/productCard/productCard.component.ts
@@ -1,15 +1,20 @@
-import { Component, input } from '@angular/core';
+import { Component, input, output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Product } from '../../product.model';
 import { Rating } from 'primeng/rating';
-import { NgOptimizedImage } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
 @Component({
   selector: 'app-product-card',
-  imports: [CommonModule, Rating, NgOptimizedImage, FormsModule],
+  imports: [CommonModule, Rating, FormsModule, ButtonModule],
   templateUrl: './productCard.component.html',
   styleUrl: './productCard.component.scss',
 })
 export class ProductCardComponent {
   product = input.required<Product>();
+  onClick = output<number>();
+
+  onViewDetails() {
+    this.onClick.emit(this.product().id);
+  }
 }
