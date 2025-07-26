eef1e07fa996e5754f175de94e4dfd0e2d748f7b
Ahmed Ashraf <a.ashraf.elbendary@gmail.com>
2025-07-26 06:10:25 +0300

    Add router outlet

--- src/app/core/layout/mainLayout.component.ts
+++ src/app/core/layout/mainLayout.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './mainLayout.component.html',
   styleUrl: './mainLayout.component.scss',
 })
