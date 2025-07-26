bd193d03e28d7b6959481309c994523d339557c7
Ahmed Ashraf <a.ashraf.elbendary@gmail.com>
2025-07-26 14:20:03 +0300

    Create Product Card Component

---
+++ src/app/features/product/components/productCard/productCard.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-product-card',
+  imports: [CommonModule],
+  templateUrl: './productCard.component.html',
+  styleUrl: './productCard.component.scss',
+})
+export class ProductCardComponent {}
