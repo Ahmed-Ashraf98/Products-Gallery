47da47ecc831fdcc5c85b27ff97997de82e0e1e9
Ahmed Ashraf <a.ashraf.elbendary@gmail.com>
2025-07-26 14:38:04 +0300

    Add Product Card Design

--- src/app/features/product/components/productCard/productCard.component.ts
+++ src/app/features/product/components/productCard/productCard.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { Product } from '../../product.model';
+import { Rating } from 'primeng/rating';
+import { NgOptimizedImage } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-product-card',
-  imports: [CommonModule],
+  imports: [CommonModule, Rating, NgOptimizedImage, FormsModule],
   templateUrl: './productCard.component.html',
   styleUrl: './productCard.component.scss',
 })
-export class ProductCardComponent {}
+export class ProductCardComponent {
+  product = input.required<Product>();
+}
